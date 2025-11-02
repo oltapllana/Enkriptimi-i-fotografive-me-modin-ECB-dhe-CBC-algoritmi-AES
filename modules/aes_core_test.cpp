cc14aafdc47504302bdc904e5da3ca1067e86b62
Altin Pajaziti <altinpajaziti935@gmail.com>
2025-11-02 12:11:25 +0100

    Add PKCS7 padding function to AES core test module

--- modules/aes_core_test.cpp
+++ modules/aes_core_test.cpp
@@ -54,3 +54,15 @@ void AESCore::keyExpansion() {
         }
     }
  }
+
+
+
+ 
+#include <stdexcept>
+
+static std::vector<uint8_t> padPKCS7(const std::vector<uint8_t>& data) {
+    size_t padding = 16 - (data.size() % 16);
+    std::vector<uint8_t> out = data;
+    out.insert(out.end(), padding, static_cast<uint8_t>(padding));
+    return out;
+}
